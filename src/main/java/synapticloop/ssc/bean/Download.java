813e1f492678310df5197f24d355db5ec11d5a09
synapticloop <contact@synapticloop.com>
2014-01-27 20:53:09 +0000

    refactored commenting into download

--- src/main/java/synapticloop/ssc/bean/Download.java
+++ src/main/java/synapticloop/ssc/bean/Download.java
@@ -1,6 +1,14 @@
 package synapticloop.ssc.bean;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
+
+import synapticloop.ssc.utils.SetupManager;
+
 public class Download {
+	private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
+
 	private String sabNzbId = null;
 	private String failMessage = null;
 	private long completedTime = 0l;
@@ -26,4 +34,29 @@ public class Download {
 	public String getGuid() { return guid; }
 	public boolean getIgnored() { return ignored; }
 	public String getName() { return name; }
+	public String getComment() {
+			SetupManager setupManager = SetupManager.INSTANCE;
+			String format = null;
+			if(getIsFailed()) {
+				format = setupManager.getFailedCommentFormat();
+			} else {
+				format = setupManager.getSuccessCommentFormat();
+			}
+
+			// now that we have the correct format
+			StringBuilder servers = new StringBuilder();
+
+			for (Iterator<String> iterator = setupManager.getNewznabServers().iterator(); iterator.hasNext(); ) {
+				String server = (String) iterator.next();
+				servers.append(server);
+				if(iterator.hasNext()) {
+					servers.append(", ");
+				}
+			}
+			format = format.replaceAll("%DATE%", SIMPLE_DATE_FORMAT.format(new Date(completedTime)));
+			format = format.replaceAll("%MESSAGE%", failMessage);
+			format = format.replaceAll("%SERVERS%", servers.toString());
+			
+			return(format);
+	}
 }
